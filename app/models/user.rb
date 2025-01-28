7c1c81e12d61c8c63573759ab7cfe697b61b5895
Martin Arning <158460540+mfa3000@users.noreply.github.com>
2025-01-28 12:21:22 +0100

    added relations in models

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,15 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  has_many :roasteries
+  has_many :beans
+  has_many :roastery_comments
+  has_many :bean_comments
+  has_many :roastery_reviews
+  has_many :bean_reviews
+  has_many :favourite_roasteries
+  has_many :favourite_beans
+  has_many :roastery_comment_votes
+  has_many :bean_comment_votes
+
 end
