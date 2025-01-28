6ffe7792b2046bdfc54d84ef239b3907f795c0c7
Martin Arning <158460540+mfa3000@users.noreply.github.com>
2025-01-28 17:02:59 +0100

    updated roasteries-show, beans-show created

--- app/controllers/beans_controller.rb
+++ app/controllers/beans_controller.rb
@@ -1,2 +1,17 @@
 class BeansController < ApplicationController
+  before_action :set_bean, only: [:show]
+
+  def show
+  end
+
+  def index
+    @bean = Bean.all
+  end
+
+private
+
+  def set_bean
+    @bean = Bean.find(params[:id])
+  end
+
 end
