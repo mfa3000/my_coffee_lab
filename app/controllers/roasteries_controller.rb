c3ea1b75507ece336bbceecc749d74895b454d40
Martin Arning <158460540+mfa3000@users.noreply.github.com>
2025-01-28 16:30:56 +0100

    Roastery Route Controller View

--- app/controllers/roasteries_controller.rb
+++ app/controllers/roasteries_controller.rb
@@ -1,2 +1,13 @@
 class RoasteriesController < ApplicationController
+  before_action :set_roastery, only: [:show]
+  def show
+
+  end
+
+private
+
+  def set_roastery
+    @roastery = Roastery.find(params[:id])
+  end
+
 end
