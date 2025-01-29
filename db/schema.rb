3e9d3e510ef120e083c56ff433b417f76c65cb39
felixdelamare <delamare.felix@gmail.com>
2025-01-29 12:33:07 +0100

    start working on comment section

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_01_28_141444) do
+ActiveRecord::Schema[7.1].define(version: 2025_01_29_105354) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -89,6 +89,7 @@ ActiveRecord::Schema[7.1].define(version: 2025_01_28_141444) do
     t.string "name"
     t.text "description"
     t.string "image"
+    t.string "address"
     t.string "roastery_url"
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
