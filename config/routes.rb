471180ac697bc8ac3a2174ee7b4319ef120121b6
felixdelamare <delamare.felix@gmail.com>
2025-01-29 13:01:21 +0100

    start working on comment section

--- config/routes.rb
+++ config/routes.rb
@@ -22,6 +22,8 @@ Rails.application.routes.draw do
     collection do
       get 'search'
     end
+    
+    resources :roastery_comments, only: [:create]
   end
 
   resources :beans, only: [:index, :new, :create, :show]
