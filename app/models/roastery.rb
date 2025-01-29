c48d4a9ffc63eca25b444c7887b5a7f84d4d287d
Chloe Lavat <lavat.chlo@gmail.com>
2025-01-29 16:07:31 +0100

    filters roastery

--- app/models/roastery.rb
+++ app/models/roastery.rb
@@ -9,4 +9,7 @@ class Roastery < ApplicationRecord
 
   validates :name, presence: true
   validates :description, presence: true
+
+  geocoded_by :address  
+  after_validation :geocode, if: ->(obj){ obj.address.present? && obj.latitude.nil? }
 end
