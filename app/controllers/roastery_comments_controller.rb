ddd982dec8eba0e66c6e3d7694f288b0015336ea
felixdelamare <delamare.felix@gmail.com>
2025-01-29 16:57:21 +0100

    Roasteries comments with Stimulus

--- app/controllers/roastery_comments_controller.rb
+++ app/controllers/roastery_comments_controller.rb
@@ -8,11 +8,20 @@ class RoasteryCommentsController < ApplicationController
 
     if @roastery_comment.save
       respond_to do |format|
-        format.turbo_stream
+        format.turbo_stream do
+          render turbo_stream: turbo_stream.append(
+            "roastery_#{@roastery.id}_comments", # Matches the div ID
+            partial: "roastery_comments/roastery_comment",
+            locals: { roastery_comment: @roastery_comment }
+          )
+        end
         format.html { redirect_to @roastery, notice: "Comment added!" }
       end
     else
-      redirect_to @roastery, alert: "Failed to add comment."
+      respond_to do |format|
+        format.turbo_stream { render turbo_stream: turbo_stream.replace("comment_form", partial: "roastery_comments/form", locals: { roastery_comment: @roastery_comment }) }
+        format.html { redirect_to @roastery, alert: "Failed to add comment." }
+      end
     end
   end
 
