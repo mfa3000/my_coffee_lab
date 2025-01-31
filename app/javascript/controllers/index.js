5fa5d4b4ff39e8902005148e93e49d770546a709
Martin Arning <158460540+mfa3000@users.noreply.github.com>
2025-01-31 16:54:05 +0100

    changed index.js

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -1,8 +1,5 @@
 // Import and register all your controllers from the importmap via controllers/**/*_controller
 import { application } from "./application";
 import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading";
-import FavouriteController from "controllers/favourite_controller";
-import LocationController from "./location_controller";
-import RoasterySearchController from "./roastery_search_controller";
 
 eagerLoadControllersFrom("controllers", application);
