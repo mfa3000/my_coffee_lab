ab302120ef1bd2f8968e3ef697f7097d8fa48149
Martin Arning <158460540+mfa3000@users.noreply.github.com>
2025-02-06 09:34:49 +0100

    commented out has many on user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,9 @@
 class UsersController < ApplicationController
   # before_a has_many :favourite_beans, dependent: :destroy
 
-  has_many :favourite_beans, dependent: :destroy
-  has_many :favourite_beans, through: :favourite_beans, source: :bean
-  
+  # has_many :favourite_beans, dependent: :destroy
+  # has_many :favourite_beans, through: :favourite_beans, source: :bean
+
   def profile
     @user = current_user
     @favourites = {
